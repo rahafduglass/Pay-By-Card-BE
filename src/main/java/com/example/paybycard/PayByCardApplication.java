fc2349f05ee154b29c47b6578ef54af5f9626061
ps.rahaf.mohammed <rahaf.mohammed@progressoft.io>
2025-04-14 14:59:13 +0300

    feat: project structure

---
+++ src/main/java/com/example/paybycard/PayByCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.paybycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayByCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayByCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paybycard/PayByCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paybycard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayByCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
