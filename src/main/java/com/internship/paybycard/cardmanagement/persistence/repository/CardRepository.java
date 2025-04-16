2de97877f02e284748277c78ce8bcfb9c2628e7b
ps.rahaf.mohammed <rahaf.mohammed@progressoft.io>
2025-04-16 18:45:57 +0300

    feat: setting persistance logic for Cards

---
+++ src/main/java/com/internship/paybycard/cardmanagement/persistence/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package com.internship.paybycard.cardmanagement.persistence.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository {
+}
--- src/test/java/com/internship/paybycard/PayByCardApplicationTests.java
+++ src/test/java/com/internship/paybycard/PayByCardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.paybycard;
+package com.internship.paybycard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
