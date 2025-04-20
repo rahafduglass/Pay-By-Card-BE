0157df0926f645f5b72783e50d97b5cce5dfd1d5
Husam Hisham <husam.hisham@progressoft.com>
2025-04-20 16:33:47 +0300

    chore: add todos

--- core/src/main/java/com/internship/paybycard/core/mapper/CardMapper.java
+++ core/src/main/java/com/internship/paybycard/core/mapper/CardMapper.java
@@ -4,4 +4,5 @@ public interface CardMapper<R,M> {
      R mapToModel(M card);
 
      M reverseMap(R reverseCard);
+//     todo Rename both methods, u shouldn't be coupled with specific class since this is interface for all mappers
 }
