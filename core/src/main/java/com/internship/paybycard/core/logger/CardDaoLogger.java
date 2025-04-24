4638fa58c8b539f9faedf802877b2c87bc97a935
ps.rahaf.mohammed <rahaf.mohammed@progressoft.io>
2025-04-24 22:30:38 +0300

    feat: deleted core interfaces

--- core/src/main/java/com/internship/paybycard/core/logger/CardDaoLogger.java
+++
@@ -1,8 +0,0 @@
-package com.internship.paybycard.core.logger;
-
-public interface CardDaoLogger {
-
-    void info(String message);
-    void error(String message);
-    void debug(String message);
-}
