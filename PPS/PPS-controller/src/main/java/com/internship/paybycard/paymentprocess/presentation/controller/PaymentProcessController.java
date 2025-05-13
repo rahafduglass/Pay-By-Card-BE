e1d3bb70d2ca5df4a044ea4d43ced9a4ce4fa789
Husam Hisham <husam.hisham@progressoft.com>
2025-05-13 16:34:23 +0300

    fix: refactor

--- PPS/PPS-controller/src/main/java/com/internship/paybycard/paymentprocess/presentation/controller/PaymentProcessController.java
+++ PPS/PPS-controller/src/main/java/com/internship/paybycard/paymentprocess/presentation/controller/PaymentProcessController.java
@@ -16,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
 @RestController
 @RequestMapping("api/v1/payment-process")
 @RequiredArgsConstructor
@@ -27,17 +29,13 @@ public class PaymentProcessController {
     @PostMapping("/initiate")
     public ResponseEntity<InitiatePaymentResponse> initiatePayment(@RequestBody @Valid InitiatePaymentCommandImpl command) {
         Result<InitiatePaymentUseCaseResponse> result = paymentProcessUseCase.initiatePayment(command);
-        if (result.getStatus()== Status.RJC) {
-            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .body(new InitiatePaymentResponse(result.getErrorCode().toString(), null));
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(paymentFormatter.toInitiatePaymentResponse(result.getData()));
+        return paymentFormatter.toInitiatePaymentResponse(result);
     }
 
     @PutMapping("/verify")
     public ResponseEntity<Void> verifyPayment(@RequestBody @Valid VerifyPaymentCommandImpl command) {
         Result<Void> result = paymentProcessUseCase.verifyPayment(command);
-        if (result.getStatus()== Status.RJC) {
+        if (result.getStatus() == Status.RJC) {
             return rejectResponse(result.getErrorCode());
         }
         return ResponseEntity.noContent().build();
@@ -46,7 +44,7 @@ public class PaymentProcessController {
     @PutMapping("/complete")
     public ResponseEntity<Void> completePayment(@RequestBody @Valid CompletePaymentCommandImpl command) {
         Result<Void> result = paymentProcessUseCase.completePayment(command);
-        if (result.getStatus()== Status.RJC) {
+        if (result.getStatus() == Status.RJC) {
             return rejectResponse(result.getErrorCode());
         }
         return ResponseEntity.noContent().build();
@@ -55,7 +53,7 @@ public class PaymentProcessController {
     private <T> ResponseEntity<T> rejectResponse(ErrorCode errorCode) {
         return switch (errorCode) {
             case INVALID_CARD, PAYMENT_NOT_FOUND, INSUFFICIENT_CARD_BALANCE, INVALID_OR_EXPIRED_OTP ->
-                    ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+                    ResponseEntity.status(UNPROCESSABLE_ENTITY).build();
             default -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         };
     }
--- PPS/PPS-controller/src/main/java/com/internship/paybycard/paymentprocess/presentation/formatter/PaymentFormatter.java
+++ PPS/PPS-controller/src/main/java/com/internship/paybycard/paymentprocess/presentation/formatter/PaymentFormatter.java
@@ -1,12 +1,32 @@
 package com.internship.paybycard.paymentprocess.presentation.formatter;
 
 import com.internship.paybycard.paymentprocess.core.domain.dto.payment.response.InitiatePaymentUseCaseResponse;
+import com.internship.paybycard.paymentprocess.core.domain.result.Result;
+import com.internship.paybycard.paymentprocess.core.domain.result.Status;
 import com.internship.paybycard.paymentprocess.presentation.dto.response.InitiatePaymentResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @Component
 public class PaymentFormatter {
-    public InitiatePaymentResponse toInitiatePaymentResponse(InitiatePaymentUseCaseResponse result) {
-        return new InitiatePaymentResponse(result.getMessage(), result.getReferenceNumber());
+    public ResponseEntity<InitiatePaymentResponse> toInitiatePaymentResponse(Result<InitiatePaymentUseCaseResponse> result) {
+        if (result.getStatus().equals(Status.ACT)) {
+            InitiatePaymentResponse initiatePaymentResponse = new InitiatePaymentResponse(result.getData().getMessage(), result.getData().getReferenceNumber());
+            return createInitiateResponseEntity(CREATED, initiatePaymentResponse);
+        } else {
+            InitiatePaymentResponse rejectedResponse = new InitiatePaymentResponse(result.getErrorCode().toString(), null);
+            return createInitiateResponseEntity(UNPROCESSABLE_ENTITY, rejectedResponse);
+        }
     }
+
+    private ResponseEntity<InitiatePaymentResponse> createInitiateResponseEntity(HttpStatus httpStatus, InitiatePaymentResponse body) {
+        return ResponseEntity
+                .status(httpStatus)
+                .body(body);
+    }
+
 }
