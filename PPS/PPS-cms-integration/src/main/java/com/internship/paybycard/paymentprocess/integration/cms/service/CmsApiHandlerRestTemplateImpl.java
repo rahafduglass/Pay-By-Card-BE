0dbf3846b86d56d92b3bf313e7f56bf7e10ce353
ps.rahaf.mohammed <rahaf.mohammed@progressoft.io>
2025-05-19 14:37:55 +0300

    fix: removing jwt code

--- PPS/PPS-cms-integration/src/main/java/com/internship/paybycard/paymentprocess/integration/cms/service/CmsApiHandlerRestTemplateImpl.java
+++ PPS/PPS-cms-integration/src/main/java/com/internship/paybycard/paymentprocess/integration/cms/service/CmsApiHandlerRestTemplateImpl.java
@@ -9,16 +9,12 @@ import com.internship.paybycard.paymentprocess.core.integration.cms.service.CmsA
 import com.internship.paybycard.paymentprocess.integration.cms.config.CmsApiProperties;
 import com.internship.paybycard.paymentprocess.integration.cms.dto.CardApiResponse;
 import com.internship.paybycard.paymentprocess.integration.cms.dto.CmsApiWithdrawRequest;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -27,7 +23,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.security.Key;
-import java.util.Date;
 
 @Component
 @Slf4j
@@ -39,11 +34,6 @@ public class CmsApiHandlerRestTemplateImpl implements CmsApiHandler {
     private final RestTemplate restTemplate;
 
 
-    @Value("${jwt.secret-key}")
-    @Setter
-    @Getter
-    private String secretKey;
-
     public CmsApiHandlerRestTemplateImpl(CmsApiProperties cmsApiProperties) {
         this.restTemplate = new RestTemplate();
         this.cmsApiProperties = cmsApiProperties;
@@ -51,23 +41,10 @@ public class CmsApiHandlerRestTemplateImpl implements CmsApiHandler {
 
     @Override
     public CardDto verifyCard(VerifyCardDto verifyCardDto) {
-        Key secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode( getSecretKey()));
-
         try {
-            long oneDayFromNow = System.currentTimeMillis() + 86400000L;
-            String jwt = Jwts.builder().setSubject("payment-service-request")
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(oneDayFromNow))
-                    .signWith(SignatureAlgorithm.HS256,secretKey)
-                    .compact();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(jwt);
-            HttpEntity<VerifyCardDto> request = new HttpEntity<>(verifyCardDto, headers);
-
             ResponseEntity<CardApiResponse> response = restTemplate.postForEntity(
                     cmsApiProperties.getBaseUrl() + "/api/v1/cards/validate",
-                    request,
+                    verifyCardDto,
                     CardApiResponse.class
             );
             if (response.getBody() == null) {
@@ -75,10 +52,10 @@ public class CmsApiHandlerRestTemplateImpl implements CmsApiHandler {
             }
             return response.getBody().getData();
         } catch (HttpClientErrorException e) {
-            if(e.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY) {
+            if (e.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY) {
                 throw new InvalidCardException("Client error: " + e.getResponseBodyAsString(), ErrorCode.INVALID_CARD);
             }
-            throw new RuntimeException("Client error: " +e.getStatusCode()+ e.getResponseBodyAsString());
+            throw new RuntimeException("Client error: " + e.getStatusCode() + e.getResponseBodyAsString());
         } catch (Exception e) {
             throw new RuntimeException("Error during CMS API call", e);
         }
@@ -98,5 +75,4 @@ public class CmsApiHandlerRestTemplateImpl implements CmsApiHandler {
             throw new RuntimeException("Error during CMS API withdraw call", e);
         }
     }
-
 }
